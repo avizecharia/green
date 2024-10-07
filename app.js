2abf6fd833bae7b16c71794ac7259434ebd8a815
aviel <aviel469@gmail.com>
2024-10-07 12:42:59 +0300

    begining of user model

---
+++ app.js
@@ -0,0 +1,10 @@
+const exp = require("express");
+const dotenv = require("dotenv");
+const app = exp();
+dotenv.config();
+const port = process.env.PORT || 1415;
+app.use(exp.json());
+app.use("/auth", require("./routers/authRouter"));
+app.use("/user", require("./routers/userRouter"));
+app.use("/greenEye", require("./routers/greenEyeRouter"));
+app.listen(port, () => console.log(`server running on port ${port}`));
