da0513ca63481051d8d9c56be33177fa738d5285
aviel <aviel469@gmail.com>
2024-10-07 15:15:47 +0300

    bouth model done

--- green-eye/models/userModel.js
+++ green-eye/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+export const userSchema = new mongoose.Schema({
   user_name: {
     type: String,
     required: [true, "user name is required"],
@@ -15,6 +15,23 @@ const userSchema = new mongoose.Schema({
     enum: ["soldier", "commander"],
     required: [true, "please provide a role"],
   },
+  area: {
+    type: String,
+    enum: ["center", "north", "south", "west", "east"],
+    required: [true, "please provide a area"],
+  },
+  units: {
+    type: [Number],
+    required: [true, "please specify unit"],
+  },
 });
 
 const UserModel = mongoose.model("user", userSchema);
+
+
+module.exports = {
+    UserModel,
+    userSchema
+};
+
+
