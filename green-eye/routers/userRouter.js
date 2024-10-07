995803143293c4660c541e05571864871e03438c
aviel <aviel469@gmail.com>
2024-10-07 17:35:07 +0300

    protect all the protecte routh

--- green-eye/routers/userRouter.js
+++ green-eye/routers/userRouter.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
-const { register, getProfile, setSettings } = require('../controllers/userController')
+const { register, getProfile, setSettings } = require('../controllers/userController');
+const { onlySoldiersAndCommanders } = require("../middllewares/authMiddlewares");
 
 router.post('/register', register)
 
-router.get('/profile', getProfile)
+router.get('/profile',onlySoldiersAndCommanders, getProfile)
 
-router.patch('/settings', setSettings)
+router.patch('/settings',onlySoldiersAndCommanders, setSettings)
 
 module.exports = router;
